9da350a87e828a7c8207a013a540a5f31e7b37ec
chihiro <ttuyduy113@gmail.com>
2017-11-10 17:12:03 +1100

    added post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,8 @@ from blog import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    #r beginning
+    # ^post/: after beginning
+    # (\d+) : pattern digit repeat
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +12,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # passing those posts to template, put into {}
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
