703c27ec950dc4a10d7d8d019592638b3250f5f4
chihiro <ttuyduy113@gmail.com>
2017-11-13 10:33:02 +1100

    Added views to create and edit blog post

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     # ^post/: after beginning
     # (\d+) : pattern digit repeat
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
